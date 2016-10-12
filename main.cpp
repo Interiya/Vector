668efed5cb2e079a17cabf91731020d995268c16
Interiya <izumrud92@mail.ru>
2016-10-12 20:04:07 +1000

    add gtest

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
-
 #include "Vector.h"
+#include "gtest/gtest.h"
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+TEST(Vector,EQ){
+    Vector<int> a(5);
+    Vector<int> b(5);
+    a.pushBack(1);
+    b.pushBack(1);
+    ASSERT_EQ(a,b);
+};
+
+int main(int argc, char *argv[]){
+
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+
 }
